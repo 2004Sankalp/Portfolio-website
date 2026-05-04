d1e9f9eedfab2731d84ba120c75200192313a586
2004Sankalp <2004sankalp@gmail.com>
2026-05-05 01:52:10 +0530

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/portfolio-website/',
+    base: '/Portfolio-website/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
